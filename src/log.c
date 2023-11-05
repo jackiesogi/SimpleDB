6ea15327a99780e87be80180fefe3c99364a315e
jackiesogi <jackiesogi@gmail.com>
2023-11-05 15:37:31 +0800

    add new print function that have color

--- src/log.c
+++ src/log.c
@@ -1,7 +1,16 @@
 #include "../include/log.h"
+#include "../include/cli.h"
 #include <stdio.h>
 
 void log_message(const char* message)
 {
+    printf(ANSI_COLOR_RESET);
     printf("%s\n", message);
+}
+
+void log_error(const char* message)
+{
+    printf(ANSI_COLOR_RED);
+    printf("%s\n", message);
+    printf(ANSI_COLOR_RESET);
 }
