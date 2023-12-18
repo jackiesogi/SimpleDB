2d3bf8c55458f11c9b0fffe676e34022c80c4ffe
jackiesogi <jackiesogi@gmail.com>
2023-12-18 16:20:59 +0800

    update the function to fix the 'two new line' bug

--- src/log.c
+++ src/log.c
@@ -1,16 +1,24 @@
 #include "../include/log.h"
 #include "../include/cli.h"
 #include <stdio.h>
+#include <string.h>
 
 void log_message(const char* message)
 {
     printf(ANSI_COLOR_RESET);
-    printf("%s\n", message);
+    if(strncmp(message, "", 2) == 0)
+    {
+      printf("%s", message);
+    }
+    else
+    {
+      printf("%s\n", message);
+    }
 }
 
 void log_error(const char* message)
 {
     printf(ANSI_COLOR_RED);
-    printf("%s\n", message);
+    fprintf(stderr, "%s\n", message);
     printf(ANSI_COLOR_RESET);
 }
